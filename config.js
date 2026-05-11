28499fde63f5fab1ef6daafad46dc909d37fc457
Jon Garcia <caldualtti@tubacex.com>
2026-05-11 11:01:30 +0200

    Creedenciales supabase

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 // 3. Guarda este archivo y despliega
 // ================================================
 
-window.SUPABASE_URL      = 'TU_SUPABASE_URL';       // ej: https://abcdefgh.supabase.co
-window.SUPABASE_ANON_KEY = 'TU_SUPABASE_ANON_KEY';  // ej: eyJhbGci...
+window.SUPABASE_URL      = 'https://ebtrcytywciyisfaetkc.supabase.co/rest/v1/';       // ej: https://abcdefgh.supabase.co
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVidHJjeXR5d2NpeWlzZmFldGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NjY5MTksImV4cCI6MjA5NDA0MjkxOX0.e0Kwbif7Ksy7jvaPtDvpuJoDySdyzWDhWL9Bv-eW-2A';  // ej: eyJhbGci...
